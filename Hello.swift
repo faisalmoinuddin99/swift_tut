849beff84980628f7eea101f2fda73224948b787
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-03-10 09:47:38 +0530

    Arrays

--- Hello.swift
+++ Hello.swift
@@ -1,1 +1,17 @@
-print("Hello, World!")
+var fruits : [String] = ["🍉","🍎","🍑","🍇"] 
+
+print(fruits.count)
+print(fruits.first)
+print(fruits.last)
+print(fruits[2])
+
+fruits.append("🍐")
+fruits.insert("🍒", at: 3)
+print("FRUITS",fruits)
+
+fruits.sort()
+print("SORT: ",fruits)
+fruits.reverse()
+print("REVERSE: ",fruits)
+fruits.shuffle()
+print("shuffle: ",fruits)

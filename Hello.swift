c8e67615fcb55ea20fecd3cc9baf47657b98befe
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-03-10 10:29:49 +0530

    Dictionary

--- Hello.swift
+++ Hello.swift
@@ -1,29 +1,13 @@
-// SETs
+// Dictionary
 
+// key: value pair
 
-var ages = [18,33,55,17,94,26,33,17]
+let devices : [String: String] = [
+   "phone" : "iPhone X",
+   "laptop" : "2016 MacBook Pro",
+   "tablet" : "2018 iPad Pro",
+   "desktop" : "2017 iMac Pro",
+    
+    ]
 
-// var agesSet : Set<Int> = []
-
-var agesSet = Set(ages)
-print(agesSet)
-
-print(agesSet.contains(55)) // true
-print(agesSet.contains(515)) // false
-
-// insertion
-
-agesSet.insert(101)
-print(agesSet) // [17, 101, 94, 26, 33, 55, 18]
-
-print(agesSet.contains(101)) // true
-
-
-/*
-Set is unordered whereas arrays are ordered
-Sets dont allow duplicate values if contains then complier ignors duplicate values, but arrays allows duplicate values
-Performance wise Set is much higher as compare to Arrays, because Set's internally uses Hashing technique to store the values
-
-So if you are dealing with faster insertion or faster deletion then go for Set's because internally Sets uses hashing hence we get 
-performance boostup
-*/
+   print(devices["laptop"]) // constant time -- very fast 

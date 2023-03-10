aa073c8ebfda14f6ace72c2314775e103271abc0
faisalmoinuddin99 <faisal25march99@gmail.com>
2023-03-10 10:02:06 +0530

    Set

--- Hello.swift
+++ Hello.swift
@@ -1,17 +1,29 @@
-var fruits : [String] = ["🍉","🍎","🍑","🍇"] 
+// SETs
 
-print(fruits.count)
-print(fruits.first)
-print(fruits.last)
-print(fruits[2])
 
-fruits.append("🍐")
-fruits.insert("🍒", at: 3)
-print("FRUITS",fruits)
+var ages = [18,33,55,17,94,26,33,17]
 
-fruits.sort()
-print("SORT: ",fruits)
-fruits.reverse()
-print("REVERSE: ",fruits)
-fruits.shuffle()
-print("shuffle: ",fruits)
+// var agesSet : Set<Int> = []
+
+var agesSet = Set(ages)
+print(agesSet)
+
+print(agesSet.contains(55)) // true
+print(agesSet.contains(515)) // false
+
+// insertion
+
+agesSet.insert(101)
+print(agesSet) // [17, 101, 94, 26, 33, 55, 18]
+
+print(agesSet.contains(101)) // true
+
+
+/*
+Set is unordered whereas arrays are ordered
+Sets dont allow duplicate values if contains then complier ignors duplicate values, but arrays allows duplicate values
+Performance wise Set is much higher as compare to Arrays, because Set's internally uses Hashing technique to store the values
+
+So if you are dealing with faster insertion or faster deletion then go for Set's because internally Sets uses hashing hence we get 
+performance boostup
+*/
